7a976cbf34be074e7626ead3db59b616bf55b298
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2019-12-11 15:43:48 +0530

    create spring boot project

---
+++ src/main/java/com/bishal/bulk/export/BulkExportApplication.java
@@ -0,0 +1,13 @@
+package com.bishal.bulk.export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bishal/bulk/export/BulkExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.bulk.export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
